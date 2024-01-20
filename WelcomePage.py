cf39c89c2676e241bc81d193209b0c87d8bd49e0
laraxl <laraconcordia1@gmail.com>
2024-01-20 12:39:54 -0500

    welcome page

--- WelcomePage.py
+++ WelcomePage.py
@@ -1,2 +1,2 @@
 print("Welcome to the DRW Open Data Visualization Challenge!")
-print("----------------------------------------------------")
+print("------------------------------------------------------")
